c36a49d46531baea61799b004f80e93708324421
Marcos Cordeiro <marcos.cordeiro@cedrotech.com>
2020-05-04 00:36:30 -0300

    Create ExcelMappingWithItems.cs

---
+++ EPPlusExtensions/ExcelMappingWithItems.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace EPPlusExtensions
+{
+    public class ExcelMappingWithItems<T> : ExcelMapping<T>
+    {
+        public ExcelMappingWithItems(IEnumerable<T> items)
+        {
+            Items = items;
+        }
+
+        private IEnumerable<T> Items { get; }
+
+        public byte[] WriteExcelFile(bool autoFit = true)
+        {
+            return WriteExcelFile(Items, autoFit);
+        }
+    }
+}
